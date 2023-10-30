792c9d29761385ac2d563ed55b1c479eaff1ed57
auzieuji <dhiaauziecantika0212@gmail.com>
2023-10-30 09:05:21 +0700

    calcu

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const app = Vue.createApp({
             text: 'Calculator Sederhana dengan VueJS',
             v1: '',
             v2: '',
-            texthasil: 'Hasil= ',
+            texthasil: 'Hasil =',
             hasil: 0
+        }
+    },
+    methods: {
+        calculate(operator) {
+            if (operator === '+') {
+                this.hasil = parseFloat(this.v1) + parseFloat(this.v2);
+            } else if (operator === '-') {
+                this.hasil = parseFloat(this.v1) - parseFloat(this.v2);
+            } else if (operator === '*') {
+                this.hasil = parseFloat(this.v1) * parseFloat(this.v2);
+            } else if (operator === '/') {
+                this.hasil = parseFloat(this.v1) / parseFloat(this.v2);
+            }
         }
     }
 })
-app.mount ('#app')
+
+app.mount('#app')
